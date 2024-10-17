86bd12713311c3c8dd76ed83abd97e6c296b21c9
Mariana <toretimariana@gmail.com>
2024-10-17 20:42:17 -0300

    ordem alfabética

--- data.js
+++ data.js
@@ -8,6 +8,24 @@ const termsData = [
     gif: "gif/-.gif"
 }*/
     {
+        name: "Ácido",
+        categories: ["QGe"],
+        description: "Ácido é aquela substância que tem capacidade de doar íons de hidrogénio (H<sup>+</sup>) em solução aquosa.",
+        gif: "gif/Acido.gif"
+    },
+    {
+        name: "Átomos",
+        categories: ["QGe"],
+        description: "Átomo é uma unidade básica de matéria que consiste num núcleo central de carga elétrica positiva envolto por uma nuvem de elétrons de carga negativa.",
+        gif: "gif/Atomos.gif"
+    },
+    {
+        name: "Básico/Alcalino",
+        categories: ["QGe"],
+        description: "Básico é aquela substãncia que libertam íons (OH<sup>-</sup>) em meio aquoso.",
+        gif: "gif/Basico.gif"
+    },
+    {
         name: "Carbono",
         categories: ["Org", "Ele"],
         description: "Carbono é o elemento com número atômico 6.",
@@ -20,31 +38,35 @@ const termsData = [
         gif: "gif/Elemento_quimico.gif"
     },
     {
+        name: "Gasoso",
+        categories: ["QGe"],
+        description: "Quando há maior afastamento das moléculas surge o estado gasoso (átomos dispersos).",
+        gif: "gif/Gasoso.gif"
+    },
+    {
+        name: "Íon negativo",
+        categories: ["QGe"],
+        description: "Íon negativo tem uma carga negativa. Ganhou elétron. Também chamado de ânodo.",
+        gif: "gif/Ion_negativo.gif"
+    },
+    {
+        name: "Íon positivo",
+        categories: ["QGe"],
+        description: "Íon positivo tem carga positiva. Perdeu elétron. Também chamado de cátion.",
+        gif: "gif/Ion_positivo.gif"
+    },
+    {
         name: "Laboratório Químico",
         categories: ["Lab"],
         description: "Laboratório Químico é o lugar onde os cientistas e pesquisadores vão fazer experiências e pesquisas.",
         gif: "gif/Laboratorio_quimico.gif"
     },
     {
-        name: "Química Inorgânica",
-        categories: ["Ino"],
-        description: "Química Inorgânica é a área da Química não baseada no carbono.",
-        gif: "gif/Quimica_inorganica.gif"
-    },
-    {
-        name: "Química Orgânica",
-        categories: ["Org"],
-        description: "Química Orgânica é a área da Química baseada no carbono.",
-        gif: "gif/Quimica_organica.gif"
-    },
-
-    {
-        name: "Átomos",
+        name: "Líquido",
         categories: ["QGe"],
-        description: "Átomo é uma unidade básica de matéria que consiste num núcleo central de carga elétrica positiva envolto por uma nuvem de elétrons de carga negativa.",
-        gif: "gif/Atomos.gif"
-    }
-    ,
+        description: "Quando as suas moléculas estão mais afastadas (o que dá maleabilidade).",
+        gif: "gif/Liquido.gif"
+    },
     {
         name: "Nêutron",
         categories: ["QGe"],
@@ -58,45 +80,21 @@ const termsData = [
         gif: "gif/Proton.gif"
     },
     {
-        name: "Ácido",
-        categories: ["QGe"],
-        description: "Ácido é aquela substância que tem capacidade de doar íons de hidrogénio (H<sup>+</sup>) em solução aquosa.",
-        gif: "gif/Acido.gif"
+        name: "Química Inorgânica",
+        categories: ["Ino"],
+        description: "Química Inorgânica é a área da Química não baseada no carbono.",
+        gif: "gif/Quimica_inorganica.gif"
     },
     {
-        name: "Básico/Alcalino",
-        categories: ["QGe"],
-        description: "Básico é aquela substãncia que libertam íons (OH<sup>-</sup>) em meio aquoso.",
-        gif: "gif/Basico.gif"
-    },
-    {
-        name: "Íon positivo",
-        categories: ["QGe"],
-        description: "Íon positivo tem carga positiva. Perdeu elétron. Também chamado de cátion.",
-        gif: "gif/Ion_positivo.gif"
-    },
-    {
-        name: "Íon negativo",
-        categories: ["QGe"],
-        description: "Íon negativo tem uma carga negativa. Ganhou elétron. Também chamado de ânodo.",
-        gif: "gif/Ion_negativo.gif"
-    },
-    {
-        name: "Líquido",
-        categories: ["QGe"],
-        description: "Quando as suas moléculas estão mais afastadas (o que dá maleabilidade).",
-        gif: "gif/Liquido.gif"
+        name: "Química Orgânica",
+        categories: ["Org"],
+        description: "Química Orgânica é a área da Química baseada no carbono.",
+        gif: "gif/Quimica_organica.gif"
     },
     {
         name: "Sólido",
         categories: ["QGe"],
         description: "Quando suas moléculas estão bem juntinhas (reunidas e organizadas).",
         gif: "gif/Solido.gif"
-    },
-    {
-        name: "Gasoso",
-        categories: ["QGe"],
-        description: "Quando há maior afastamento das moléculas surge o estado gasoso (átomos dispersos).",
-        gif: "gif/Gasoso.gif"
-    }   
+    }  
 ];
